4ccf8396803500edace76ec47b631b29209be99b
mahikaw <mahika16241@iiitd.ac.in>
2017-10-27 21:52:27 +0530

    Added multiplication algo

---
+++ src/algorithms/multiplication.py
@@ -0,0 +1,10 @@
+def multiply(x,y):
+	return x*y
+
+if __name__=='__main__':
+	print 'Enter first number: ',
+	x = int(raw_input())
+	print 'Enter second number: ',
+	y = int(raw_input())
+	print 'Multi: ',multiply(x,y)
+
