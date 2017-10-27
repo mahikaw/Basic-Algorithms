41d1be821a04f01b287a73186344dfd73e7ac23d
mahikaw <mahika16241@iiitd.ac.in>
2017-10-27 22:25:55 +0530

    Added division algo

---
+++ src/algorithms/division.py
@@ -0,0 +1,10 @@
+def divide(x,y):
+	return x/y
+
+if __name__=='__main__':
+	print 'Enter first number: ',
+	x = int(raw_input())
+	print 'Enter second number: ',
+	y = int(raw_input())
+	print 'Quotient: ',divide(x,y)
+
